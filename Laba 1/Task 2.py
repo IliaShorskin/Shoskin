18c1c35628e870f9a3cad7e276304f34fee011ce
IliaShorskin <shorskin.i@gmail.com>
2024-10-29 19:32:30 +0300

    Update Task 2.py

--- Laba 1/Task 2.py
+++ Laba 1/Task 2.py
@@ -1,7 +1,7 @@
 pamyat = 1.44
-a = pamyat*1024*1024		
+pamyat_v_bait = pamyat*1024*1024		
 simbols = 25*50*100*4
-b = round(a / simbols, 0)
+numbers_of_books = (pamyat_v_bait / simbols, 0)
 c = int(b)
 
 print("Количество книг, помещающихся на дискету:", c)
